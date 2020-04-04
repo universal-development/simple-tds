7fca67326a32f639eaeb580e27f48787da154893
Denis O <denis@universal-development.com>
2020-04-04 18:05:30 +0300

    Configuration loading

--- config.go
+++ config.go
@@ -6,8 +6,11 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 )
 
+const AGENT_NAME = "_agent.txt"
+const URL_NAME = "_url.txt"
 const DEFAULT_URL string = "default"
 const DEFAULT_URL_FILE string = "default_url.txt"
 
@@ -33,10 +36,17 @@ func loadConfig() {
 	for _, f := range files {
 		if f.IsDir() {
 			fmt.Println("Processing: " + f.Name())
+			var configName = f.Name()
 			var config = RedirectConfig{}
 
-			var fileContent = map[string][]string{}
+			config.name = configName
+			config.urls = map[string][]string{}
+			config.userAgentPatterns = map[string][]string{}
 
+			newRedirectConfig[configName] = config
+
+			// loading file content in a map
+			var fileContent = map[string][]string{}
 			configFiles, err := ioutil.ReadDir(configDir + "/" + f.Name())
 			if err != nil {
 				log.Fatal(err)
@@ -62,14 +72,21 @@ func loadConfig() {
 					file.Close()
 				}
 			}
-
-			log.Print(fmt.Sprintf("fileContent : %v\n", fileContent))
-
-			newRedirectConfig[f.Name()] = config
+			// extract each
+			for key, element := range fileContent {
+				if strings.HasSuffix(key, AGENT_NAME) {
+					blockName := strings.ReplaceAll(key, AGENT_NAME, "")
+					blockUrlsFile := blockName + URL_NAME
+					config.name = blockName
+					config.urls[blockName] = fileContent[blockUrlsFile]
+					config.userAgentPatterns[blockName] = element
+				}
+			}
+			config.urls[DEFAULT_URL_FILE] = fileContent[DEFAULT_URL_FILE]
 		}
 	}
 
-	log.Print(fmt.Sprintf("Loaded : %v\n", newRedirectConfig))
+	log.Print(fmt.Sprintf("Loaded config : %v\n", newRedirectConfig))
 
 	redirectConfig = newRedirectConfig
 }
