ce12c808c9ed14b7ded807683f3c0065f302f88a
Denis O <denis@universal-development.com>
2020-04-04 22:15:54 +0300

    Docker image tunes

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
-FROM scratch
+FROM alpine:3.9
+RUN apk add --no-cache libc6-compat
 
 ENV PORT 8000
 EXPOSE $PORT
 
 COPY simple-tds /
-CMD ["/simple-tds"]
+
+CMD ["./simple-tds"]
--- push.sh
+++ push.sh
@@ -5,5 +5,7 @@ set -x
 export REGISTRY=${REGISTRY:-denis256/simple-tds}
 export TAG=${TAG:-0.0.1}
 
+chmod +x ./simple-tds
+
 docker build . -t "${REGISTRY}:${TAG}"
 docker push "${REGISTRY}:${TAG}"
